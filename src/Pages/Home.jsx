142f9e088e40a1a242481ef00a42628542ab29c3
Subhashini <tjsubha2000@gmail.com>
2024-09-07 18:04:09 +0530

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,336 @@
+import { TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { Link, useNavigate } from "react-router-dom";
+import banner from "./banner.jpg";
+
+const Home = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const searchTermLower = searchTerm.toLowerCase();
+
+    if (searchTermLower === "budget") {
+      navigate("/budget");
+    } else if (
+      searchTermLower === "wedding photographer" ||
+      searchTermLower === "photographer" ||
+      searchTermLower === "photo"
+    ) {
+      navigate("/WeddingVendor/WeddingPhotographers");
+    } else if (
+      searchTermLower === "wedding videographer" ||
+      searchTermLower === "videographer" ||
+      searchTermLower === "video"
+    ) {
+      navigate("/WeddingVendor/Weddingvideographers");
+    } else if (
+      searchTermLower === "caterers" ||
+      searchTermLower === "catering"
+    ) {
+      navigate("/WeddingVendor/Caterers");
+    } else if (
+      searchTermLower === "wedding planners" ||
+      searchTermLower === "planners"
+    ) {
+      navigate("/WeddingVendor/WeddingPlanners");
+    } else if (searchTermLower === "banquet") {
+      navigate("/WeddingVenue/Banquet");
+    } else if (
+      searchTermLower === "wedding resort" ||
+      searchTermLower === "resort"
+    ) {
+      navigate("/WeddingVenue/WeddingResort");
+    } else if (
+      searchTermLower === "mandapam" ||
+      searchTermLower === "mahal" ||
+      searchTermLower === "marriage hall"
+    ) {
+      navigate("/WeddingVenue/Mandapam");
+    } else if (
+      searchTermLower === "mehndi artists" ||
+      searchTermLower === "mehndi"
+    ) {
+      navigate("/Bride/MehndiArtists");
+    } else if (
+      searchTermLower === "makeup artists" ||
+      searchTermLower === "makeup"
+    ) {
+      navigate("/Bride/MakeupArtists");
+    } else if (searchTermLower === "dress" || searchTermLower === "sherwani") {
+      navigate("/Groom/Dress");
+    } else if (
+      searchTermLower === "makeup" ||
+      searchTermLower === "groom makeup"
+    ) {
+      navigate("/Groom/GroomMakeup");
+    } else {
+      alert(`🚫🚫 ErrorMessage 🚫🚫 \nSearched Term not Found ❌❌`);
+      navigate("/");
+    }
+  };
+
+  return (
+    <div>
+      <div className="banner">
+        <img src={banner} />
+        <form
+          onSubmit={handleSearch}
+          style={{ position: "absolute", marginTop: 50 }}
+        >
+          <TextInput
+            type="text"
+            placeholder="Search the Vendor Company or Name..."
+            rightIcon={AiOutlineSearch}
+            className="w-72 bg-green-50 "
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </form>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 bg-black">
+        <div class="max-w-sm mt-10 mb-5 ml-6 mr-3 p-6 hover:bg-violet-300 bg-white border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-black">
+          <Link to="/WeddingVenue">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white hover:text-blue-500 dark:hover:text-blue-500  ">
+              Wedding Venues
+            </h5>
+          </Link>
+          <img
+            className="mt-3 h-60 card_img"
+            src="https://www.wedding-spot.com/blog/sites/wsblog/files/images/migrated/78-Fitroy%2Bwedding%2Bvenue.jpg"
+            alt="wedding image"
+          />
+          <br />
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Photos, reviews, and so much more... get in touch from here!
+          </p>
+          <br />
+          <Link
+            to="/WeddingVenue"
+            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-950 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Explore venues
+            <svg
+              class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </Link>
+        </div>
+
+        <div class="max-w-sm mt-10 mb-5 ml-6 mr-3 p-6 hover:bg-violet-300 bg-white border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-black">
+          <Link to="/WeddingVendor">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white hover:text-blue-500 dark:hover:text-blue-500  ">
+              Wedding Vendors
+            </h5>
+          </Link>
+          <img
+            className="mt-3 h-60 card_img w-96"
+            src="https://www.brides.com/thmb/x5JqyLehHlFFjfSYhwqYQkMGZZ4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1177485677-c89e88c4ce934e0092ba52f6aa6e29b9.jpg"
+            alt="Vendor image"
+          />
+          <br />
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Find the top-rated wedding vendors near you in every category.
+          </p>
+          <br />
+          <Link
+            to="/WeddingVendor"
+            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-950 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Start Searching Your Vendor
+            <svg
+              class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </Link>
+        </div>
+
+        <div class="max-w-sm mt-10 mb-5 ml-6 mr-3 p-6 hover:bg-violet-300 bg-white border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-black">
+          <Link to="/Bride">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white hover:text-blue-500 dark:hover:text-blue-500  ">
+              Bride
+            </h5>
+          </Link>
+          <img
+            className="mt-3 h-60 card_img w-96"
+            src="https://images.shaadisaga.com/shaadisaga_production/photos/pictures/000/805/153/new_medium/fotoart_by_sami_irani.jpg?1554793567"
+            alt="Bride image"
+          />
+          <br />
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            The bride is the heart of every wedding, bringing grace, beauty, and
+            joy to the occasion..
+          </p>
+
+          <Link
+            to="/Bride"
+            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-950 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Click here
+            <svg
+              class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </Link>
+        </div>
+
+        <div class="max-w-sm mt-10 mb-5 ml-6 mr-3 p-6 hover:bg-violet-300 bg-white border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-black">
+          <Link to="/Grrom">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white hover:text-blue-500 dark:hover:text-blue-500  ">
+              Groom
+            </h5>
+          </Link>
+          <img
+            className="mt-3 h-60 card_img w-96"
+            src="https://i.pinimg.com/736x/6c/05/a4/6c05a4a0d81cc20bc8980a65e226edf7.jpg"
+            alt="Groom image"
+          />
+          <br />
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Look your best on your big day with our bespoke groom attire and
+            expert grooming services!
+          </p>
+          <br />
+          <Link
+            to="/Groom"
+            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-950 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Check here
+            <svg
+              class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </Link>
+        </div>
+
+        <div class="max-w-sm mt-10 mb-5 ml-6 mr-3 p-6 hover:bg-violet-300 bg-white border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-black">
+          <Link to="/invitation">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white hover:text-blue-500 dark:hover:text-blue-500  ">
+              Invitation Card
+            </h5>
+          </Link>
+          <img
+            className="mt-3 h-60 card_img w-96"
+            src="https://myshadicards.com/wp-content/uploads/2021/09/Creative-Invitation-Card-7111-Cardset.webp"
+            alt="Groom image"
+          />
+          <br />
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Make your first impression unforgettable with our elegant and unique
+            wedding invitation cards!
+          </p>
+          <br />
+          <Link
+            to="/invitation"
+            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-950 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Explore cards
+            <svg
+              class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </Link>
+        </div>
+
+        <div class="max-w-sm mt-10 mb-5 ml-6 mr-3 p-6 hover:bg-violet-300 bg-white border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-black">
+          <Link to="/PlanningTool">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white hover:text-blue-500 dark:hover:text-blue-500  ">
+              Planning Tool
+            </h5>
+          </Link>
+          <img
+            className="mt-3 h-60 card_img w-96"
+            src="https://static.vecteezy.com/system/resources/previews/001/879/416/non_2x/planning-monthly-budget-for-shopping-and-purchase-manage-financial-plan-with-calculator-financial-consulting-software-banking-accounting-platform-illustration-of-website-banner-software-poster-free-vector.jpg"
+            alt="PlanningTool image"
+          />
+          <br />
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Custom planning tools to manage your budget and vendors.
+          </p>
+          <br />
+          <Link
+            to="/PlanningTool"
+            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-950 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Discover over Tools
+            <svg
+              class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Pages/Signin.jsx
@@ -0,0 +1,127 @@
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { HiInformationCircle } from "react-icons/hi";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import OAuth from "../Components/OAuth";
+import {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+} from "../Redux/Slice/userSlice";
+
+const Signin = () => {
+  const [formData, setFormData] = useState({});
+  const dispatch = useDispatch();
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    //console.log(e.target.value);
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+    //console.log(formData);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return dispatch(signInFailure("please fill out the fields"));
+    }
+    try {
+      dispatch(signInStart());
+      const response = await fetch(
+        "https://backend-wedding-project.onrender.com/api/auth/login-user",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      const data = await response.json();
+      if (data.success === false) {
+        return dispatch(signInFailure(data.message));
+      }
+      if (response.ok) {
+        localStorage.setItem("Token", data.token);
+        dispatch(signInSuccess(data));
+        navigate("/");
+      }
+    } catch (error) {
+      dispatch(signInFailure(error.message));
+    }
+  };
+
+  return (
+    <div className="min-h-screen mt-50">
+      <div>
+        <div>
+          <div className=" text-3xl font-semibold dark:text-white">
+            <span className=" px-2 py-1 bg-gradient-to-r from bg-pink-600 via-purple-500 to from-indigo-600 text-transparent bg-clip-text">
+              Subha Wedding Planner
+            </span>
+          </div>
+          <p className="text-m mt-6">
+            You can sign up with your Email and password or you can use
+            Google.This is an Demo Project***.
+          </p>
+        </div>
+      </div>
+      <div className="flex-1">
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+          <div>
+            <Label value="Email" />
+            <TextInput
+              type="email"
+              placeholder="name@company.com"
+              id="email"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <Label value="Password" />
+            <TextInput
+              type="password"
+              placeholder="Enter Your Password"
+              id="password"
+              onChange={handleChange}
+            />
+          </div>
+          <Button
+            gradientDuoTone="purpleToPink"
+            type="submit"
+            disabled={loading}
+          >
+            {loading ? (
+              <>
+                <Spinner
+                  color="purple"
+                  aria-label="Purple spinner example"
+                  size="sm"
+                />
+                <span className="pl-3">Loading...</span>
+              </>
+            ) : (
+              "Sign In"
+            )}
+          </Button>
+          <OAuth />
+        </form>
+        <div className="flex gap-2 text-sm mt-6">
+          <span>Don't Have An Account ?</span>
+          <Link to="/Signup" className="text-blue-500">
+            Sign Up
+          </Link>
+        </div>
+        {errorMessage && (
+          <Alert color="failure" icon={HiInformationCircle} className="mt-5">
+            <span className="font-medium me-2">🥴OOPS!</span>
+            {errorMessage}
+          </Alert>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Signin;
